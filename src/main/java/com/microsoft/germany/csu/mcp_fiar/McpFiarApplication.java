c66569cfd885546899a19c944eef676d3d22c56b
Jeffrey Groneberg <inkvine@gmx.net>
2025-05-30 14:04:10 +0200

    first commit

---
+++ src/main/java/com/microsoft/germany/csu/mcp_fiar/McpFiarApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.germany.csu.mcp_fiar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpFiarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpFiarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microsoft/germany/csu/mcp_fiar/McpFiarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.germany.csu.mcp_fiar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpFiarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
